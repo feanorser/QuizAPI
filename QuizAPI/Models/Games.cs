ad46d88a27eacba3df93127938c1ea65d6ec9c71
feansBook <feansBook@FEANSBOOKX>
2023-12-26 17:33:39 +0100

    Добавьте файлы проекта.

---
+++ QuizAPI/Models/Games.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizAPI.Models
+{
+    public class Games
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime DateTime { get; set; }
+        public string? GameType {  get; set; }
+    }
+}
