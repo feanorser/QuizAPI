c9c77732da9b1f5f27f8273ae43a55c0adea4822
Sergei Dolzhenko <feanorser@hotmail.com>
2024-01-09 11:19:10 +0100

    Initial commit. Add Teams and Games models. Switch to Guid IDs

--- QuizAPI/Models/Games.cs
+++ QuizAPI/Models/Games.cs
@@ -2,10 +2,11 @@
 {
     public class Games
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
         public DateTime DateTime { get; set; }
         public string? GameType {  get; set; }
+        public int Rounds { get; set; }
     }
 }
---
+++ QuizAPI/Models/GamesResults.cs
@@ -0,0 +1,9 @@
+﻿namespace QuizAPI.Models
+{
+    public class GamesResults
+    {
+        public Guid IdGame { get; set; }
+        public Guid IdTeam { get; set; }
+        public int[] PointsInRounds { get; set; }
+    }
+}
